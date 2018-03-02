a712697a934446574fa9e18def98a887dfca71cc
elogy <tim.beckmann@student.uni-tuebingen.de>
2018-03-02 13:54:48 +0100

    fixes URLconf in accordance with package README

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,9 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
-
